daf20620d9cf50a8822e6f09512c71eb9f867f6b
Nikita Solodovnikov <nas140301@gmail.com>
2020-12-14 22:02:40 +0300

    add marked ASTs

--- src/Language/Lambda/Untyped/AST.hs
+++ src/Language/Lambda/Untyped/AST.hs
@@ -1,17 +1,17 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Language.Lambda.Untyped.AST where
 
+import Data.Deriving
+import Data.Functor.Compose
 import Data.Functor.Foldable
+import Data.List
 import GHC.Generics
-
-data AST
-  = Var String
-  | App AST AST
-  | Lam String AST
-  deriving (Eq, Show, Generic)
 
 data ASTF a
   = VarF String
@@ -19,8 +19,44 @@ data ASTF a
   | LamF String a
   deriving (Eq, Show, Generic, Functor)
 
-type instance Base AST = ASTF
+data MarkedF t = MarkedF
+  { bound :: [String],
+    free :: [String],
+    term :: t
+  }
+  deriving (Show, Eq, Generic, Functor)
 
-instance Recursive AST
+$(deriveEq1 ''ASTF)
+$(deriveOrd1 ''ASTF)
+$(deriveShow1 ''ASTF)
+$(deriveFoldable ''ASTF)
+$(deriveTraversable ''ASTF)
 
-instance Corecursive AST
+$(deriveEq1 ''MarkedF)
+$(deriveOrd1 ''MarkedF)
+$(deriveShow1 ''MarkedF)
+$(deriveFoldable ''MarkedF)
+$(deriveTraversable ''MarkedF)
+
+type AST = Fix ASTF
+type Marked a = Fix (Compose MarkedF a)
+type MarkedAST = Marked ASTF
+
+pattern Var x = Fix (VarF x)
+pattern App f x = Fix (AppF f x)
+pattern Lam v x = Fix (LamF v x)
+
+pattern Marked b f t = Fix (Compose (MarkedF b f t))
+
+
+mark :: AST -> MarkedAST
+mark = cata collect
+  where
+    collect (VarF x) = Marked [] [x] (VarF x)
+    collect (AppF x@(Marked b1 f1 _) y@(Marked b2 f2 _))
+      = Marked (b1 `union` b2) (f1 `union` f2) (AppF x y)
+    collect (LamF v x@(Marked b f _)) = Marked (b `union` [v]) (v `delete` f) (LamF v x)
+
+unmark :: MarkedAST -> AST
+unmark = cata (Fix . term . getCompose)
+
--- src/Language/Lambda/Untyped/Eval.hs
+++ src/Language/Lambda/Untyped/Eval.hs
@@ -3,7 +3,7 @@ module Language.Lambda.Untyped.Eval where
 import Data.Functor.Foldable
 import Language.Lambda.Untyped.AST
 
-data Alpha = Alpha String AST deriving Show
+data Alpha = Alpha String AST
 
 -- TODO
 applyAlpha :: Alpha -> AST -> AST
@@ -13,7 +13,12 @@ applyAlpha (Alpha var t') = cata app
       | v == var = t'
       | otherwise = Var v
     app (AppF f x) = App f x
-    app (LamF v x) = Lam v x
+    app (LamF v x)
+      | v == var = Lam v x
 
 reduce :: AST -> AST
-reduce = _a
+reduce = ana red
+  where
+    red (App (Lam v b) x) = project $ applyAlpha (Alpha v x) b
+    red any = project any
+
