d1d9d572bb79e30a6e117150eccddeb087c9f5f6
Nikita Solodovnikov <nas140301@gmail.com>
2021-01-26 14:21:17 +0300

    use `prettyprinter`

--- app/UntypedLambda.hs
+++ app/UntypedLambda.hs
@@ -1,5 +1,8 @@
 module Main where
 
+import Data.Text.Lazy.IO qualified as T
+import Prettyprinter.Render.Text
+
 import Language.Lambda.Untyped.Eval
 import Language.Lambda.Untyped.Parser
 import Language.Lambda.Untyped.Print
@@ -8,5 +11,5 @@ import Language.Lambda.Untyped.Print
 main = do
   str <- getLine
   case parseExpr str of
-    Right expr -> putStrLn $ pretty $ reduce expr
+    Right expr -> T.putStrLn $ showAST $ reduce expr
     Left err -> error $ errorBundlePretty err
--- src/Language/Lambda/Untyped/Print.hs
+++ src/Language/Lambda/Untyped/Print.hs
@@ -1,14 +1,29 @@
 module Language.Lambda.Untyped.Print
   ( pretty
+  , showAST
   ) where
 
 import Data.Functor.Foldable
-import Text.Printf
+import Prettyprinter
+import Prettyprinter.Render.Text
+import Data.Text.Lazy qualified as T
 
 import Language.Lambda.Untyped.AST
 
-pretty :: AST -> String
-pretty = cata $ \case
-  VarF v -> v
-  AppF x y -> mconcat ["(", x, y, ")"]
-  LamF v x -> mconcat ["(\\", v, ".", x, ")"]
+instance Pretty AST where
+  pretty = para \case
+    VarF v ->
+      pretty v
+    AppF (Lam{}, x) (App{}, y) ->
+      parens x <+> parens y
+    AppF (Lam{}, x) (_, y) ->
+      parens x <+> y
+    AppF (_, x) (App{}, y) ->
+      x <+> parens y
+    AppF (_, x) (_, y) ->
+      x <+> y
+    LamF v (_, x) ->
+      mconcat [backslash, pretty v, dot] <+> x
+
+showAST :: AST -> T.Text
+showAST = renderLazy . layoutSmart defaultLayoutOptions . pretty
